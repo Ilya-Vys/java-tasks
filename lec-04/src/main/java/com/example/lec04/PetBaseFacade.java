d8739491fd503f114613abf69726139bc988e2f8
Илья Высоцкий <ilyavys100500@gmail.com>
2021-04-07 21:03:40 +0300

    lec-15 commit2

--- lec-04/src/main/java/com/example/lec04/PetBaseFacade.java
+++ lec-04/src/main/java/com/example/lec04/PetBaseFacade.java
@@ -1,22 +1,14 @@
 package com.example.lec04;
 
-import com.example.lec04.entities.Person;
 import com.example.lec04.entities.Pet;
-import com.example.lec04.entities.Sex;
 import com.example.lec04.exception.PetAlreadyExistException;
 
 public class PetBaseFacade {
 
     private PetBase base = PetBase.getInstance();
 
-    public void addPets() throws PetAlreadyExistException {
-        base.addPet(new Pet.Builder(new Person("Bob", 20, Sex.MAN), "Bobik").build());
-        base.addPet(new Pet.Builder(new Person("Bob", 30, Sex.MAN), "Tuzik").ageYears(2).build());
-        base.addPet(new Pet.Builder(new Person("Bob", 40, Sex.MAN), "Sharik").build());
-        base.addPet(new Pet.Builder(new Person("Bob", 50, Sex.WOMAN), "Frosya").ageMonths(3).build());
-        base.addPet(new Pet.Builder(new Person("Bob", 60, Sex.MAN), "Kesha").build());
-        base.addPet(new Pet.Builder(new Person("Bob", 25, Sex.WOMAN), "Murka").build());
-        base.addPet(new Pet.Builder(new Person("Bob", 35, Sex.MAN), "Kitty").build());
+    public void addPet(Pet pet) throws PetAlreadyExistException {
+        base.addPet(pet);
     }
 
     public void getPets(){
--- lec-04/src/main/java/com/example/lec04/Test.java
+++ lec-04/src/main/java/com/example/lec04/Test.java
@@ -1,5 +1,8 @@
 package com.example.lec04;
 
+import com.example.lec04.entities.Person;
+import com.example.lec04.entities.Pet;
+import com.example.lec04.entities.Sex;
 import com.example.lec04.exception.PetAlreadyExistException;
 
 public class Test {
@@ -10,8 +13,16 @@ public class Test {
 
         PetBaseFacade facade = new PetBaseFacade();
 
-        facade.addPets();
+        facade.addPet(new Pet.Builder(new Person("Bob", 20, Sex.MAN), "Bobik").build());
+        facade.addPet(new Pet.Builder(new Person("Bob", 30, Sex.MAN), "Tuzik").ageYears(2).build());
+        facade.addPet(new Pet.Builder(new Person("Bob", 40, Sex.MAN), "Sharik").build());
+        facade.addPet(new Pet.Builder(new Person("Bob", 50, Sex.WOMAN), "Frosya").ageMonths(3).build());
+        facade.addPet(new Pet.Builder(new Person("Bob", 60, Sex.MAN), "Kesha").build());
+        facade.addPet(new Pet.Builder(new Person("Bob", 25, Sex.WOMAN), "Murka").build());
+        facade.addPet(new Pet.Builder(new Person("Bob", 35, Sex.MAN), "Kitty").build());
+
         facade.getPets();
+
         facade.updatePet("Kitty", "Kittykat");
 
     }
